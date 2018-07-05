7031a6ccbb6335f05dc6f82f9865d4586ff0c763
albertparlys <albert.tambunan@lippokarawaci.co.id>
2018-07-05 17:13:22 +0700

    Update NEW.py

--- NEW.py
+++ NEW.py
@@ -1,39 +1,9 @@
 import requests  
 import datetime
 
-class BotHandler:
-
-    def __init__(self, token):
-        self.token = "617460887:AAEJ1mpQVOtBmIuSWMF0sF18VK64fjMD32w"
-        self.api_url = "https://api.telegram.org/bot{}/".format(token)
-
-    def get_updates(self, offset=None, timeout=30):
-        method = 'getUpdates'
-        params = {'timeout': timeout, 'offset': offset}
-        resp = requests.get(self.api_url + method, params)
-        result_json = resp.json()['result']
-        return result_json
-
-    def send_message(self, chat_id, text):
-        params = {'chat_id': chat_id, 'text': text}
-        method = 'sendMessage'
-        resp = requests.post(self.api_url + method, params)
-        return resp
-
-    def get_last_update(self):
-        get_result = self.get_updates()
-
-        if len(get_result) > 0:
-            last_update = get_result[-1]
-        else:
-            last_update = get_result[len(get_result)]
-
-        return last_update
-		
 greet_bot = BotHandler(token)  
 greetings = ('hello', 'hi', 'greetings', 'sup')  
-now = datetime.datetime.now()
-
+ow = datetime.datetime.now()
 
 def main():  
     new_offset = None
@@ -69,3 +39,36 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         exit()
+
+class BotHandler:
+
+    def __init__(self, token):
+        self.token = "617460887:AAEJ1mpQVOtBmIuSWMF0sF18VK64fjMD32w"
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+
+    def get_updates(self, offset=None, timeout=30):
+        method = 'getUpdates'
+        params = {'timeout': timeout, 'offset': offset}
+        resp = requests.get(self.api_url + method, params)
+        result_json = resp.json()['result']
+        return result_json
+
+    def send_message(self, chat_id, text):
+        params = {'chat_id': chat_id, 'text': text}
+        method = 'sendMessage'
+        resp = requests.post(self.api_url + method, params)
+        return resp
+
+    def get_last_update(self):
+        get_result = self.get_updates()
+
+        if len(get_result) > 0:
+            last_update = get_result[-1]
+        else:
+            last_update = get_result[len(get_result)]
+
+        return last_update
+		
+
+
+
